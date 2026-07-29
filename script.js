c8433f12abf85433ef11f0a6dd4aa71a4d859cf1
e2426323-cmd <e2426323@soka-u.jp>
2026-07-29 23:47:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,12 @@
 function message(){
-
-alert(
-"旅行を楽しむポイント\n\n" +
-"・歩きやすい靴を準備しましょう。\n" +
-"・モバイルバッテリーを持参しましょう。\n" +
-"・天気予報を確認しましょう。\n" +
-"・交通系ICカードがあると便利です。"
-);
-
+    alert("旅行を楽しみましょう！");
 }
 
 function today(){
 
-const place = [
+    const place=["東京","京都","大阪","富士山"];
 
-"東京",
+    let n=Math.floor(Math.random()*place.length);
 
-"京都",
-
-"大阪",
-
-"富士山"
-
-];
-
-const comment = [
-
-"最新のショッピングを楽しもう！",
-
-"歴史ある寺院を巡ろう！",
-
-"食べ歩きを満喫しよう！",
-
-"絶景の写真を撮ろう！"
-
-];
-
-let number = Math.floor(Math.random()*4);
-
-alert(
-
-"今日のおすすめは\n\n"
-
-+ place[number]
-
-+ "\n\n"
-
-+ comment[number]
-
-);
-
+    alert("今日のおすすめは「"+place[n]+"」です！");
 }
